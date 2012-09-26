8c6d764306461b4b3a9c8a5fb485c4adb4ecda57
nickua <ruffian.ua@gmail.com>
2012-09-26 15:42:50 +0300

    fully removed jQuery, added Prototype.js support

--- js/atwix/search.js
+++ js/atwix/search.js
@@ -1,46 +1,57 @@
-jQuery(document).ready(
-    function()
+
+var PrettyPopup = Class.create();
+
+PrettyPopup.prototype = {
+
+    getResponse: function(data)
     {
+        //Show response
+        $('search_autocomplete').innerHTML = data.responseText;
+        $('search_autocomplete').show();
+        //Restore default icon in search field
+        $('search').setStyle({background: 'white'});
+    },
+
+    showPopup: function() {
+        //hide pop-up
+        $('search_autocomplete').hide();
+        //search criteria
+        var textQuery = $(this.id).value;
+        //check if exist value
+        if (textQuery == '') {
+            return false;
+        }
+
+        //reset timer
+        clearTimeout(this.searchTimer);
+
+        var nested = this;
+
+        this.searchTimer = setTimeout(function() {
+            //show loading icon
+            $(nested.id).setStyle({ background:  "white url(" + atwixImagePath + "opc-ajax-loader.gif) no-repeat right" });
+            //AJAX request
+            new Ajax.Request(
+                atwixBasePath + "catalogsearch/ajax/suggest/",
+                {method: 'get', parameters: "q=" + textQuery, onComplete: nested.getResponse}
+            );
+
+        },750);
+        //PROFIT
+        return true;
+
+    },
+
+    initialize: function(id) {
+        this.id = id;
         //unbind system keydown action
-        $('search').stopObserving('keydown');
-        
+        $(this.id).stopObserving('keydown');
         //init timer variable
-        var searchTimer = 1;
-        
-        //new keydown action
-        jQuery("#search").keyup(function(){
-            //search criteria
-            var textQuery = jQuery("#search").val();
-            //check if exist value
-            if (textQuery == '')
-                return false;
-            
-            //reset timer
-            clearTimeout(searchTimer);
-            
-            //call function if time was expired
-            searchTimer = setTimeout(function(){
-                //show loading icon
-                jQuery("#search").css("background", "white url(" + atwixImagePath + "opc-ajax-loader.gif) no-repeat right");
-                //hide pop-up
-                jQuery("#search_autocomplete").fadeOut();
-                
-                //AJAX request
-                jQuery.get(
-                    atwixBasePath + "catalogsearch/ajax/suggest/",
-                    {q: textQuery},
-                    function(data)
-                    {
-                        //Show response
-                        jQuery("#search_autocomplete").html(data);
-                        jQuery("#search_autocomplete").fadeIn();
-                        //Restore default icon in search field
-                        jQuery("#search").css("background", "white");
-                    }
-                );
-            },750);
-            //PROFIT
-            return true;
-        });
+        this.searchTimer = 1;
+        Event.observe(this.id, 'keyup', this.showPopup.bind(this));
     }
-);
+}
+
+Event.observe(document, 'dom:loaded', function(){
+    new PrettyPopup('search');
+});
